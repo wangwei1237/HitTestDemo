c2cf7773938646e3145eff39c8f6166f925a3062
mudkip <i@mudkip.me>
2015-04-26 13:25:13 +0800

    Initial Commit

---
+++ HitTestDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HitTestDemo
+//
+//  Created by Mudkip on 15/4/26.
+//  Copyright (c) 2015年 Mudkip. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
